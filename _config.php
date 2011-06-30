029ce6b3e1801223eb3fb77a1fbea4f0978a66f6
Yevgeny Yegorov <yugene@monulent.ru>
2011-06-30 23:55:18 +0400

    Small edits for public version

--- _config.php
+++ _config.php
@@ -22,12 +22,6 @@ $cfgServers = array();
 
 $cfgServers[$i]['address'] = '192.168.0.101:4730';
 $cfgServers[$i]['name'] = '192.168.0.101';
-
-++ $i;
-
-$cfgServers[$i]['address'] = '127.0.0.1:4730';
-$cfgServers[$i]['name'] = '<localhost>';
-
 ++ $i;
 
 ?>
--- _init.php
+++ _init.php
@@ -5,20 +5,6 @@ require_once('classes/ServerList.php');
 require_once('classes/View.php');
 @include_once('Net/Gearman/Manager.php');
 
-function d($var = null)
-{
-    echo '<pre>';
-    if (! is_null($var))
-    {
-        var_dump($var);
-    }
-    else
-    {
-        debug_print_backtrace();
-    }
-    echo '</pre>';
-}
-
 $view = new GA_View();
 $view->setTemplateDir('templates');
 $view->servers = $cfgServers;
