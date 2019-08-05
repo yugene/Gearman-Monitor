77339bfe4527312171501012a4095e12005f4c25
Raphael Giovanini <raphaelgiovanini@gmail.com>
2019-08-05 15:39:29 -0300

    Change sort by

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var chart;
 var jsGraphTime = 2000;
 var jsTableTime = 5000;
-if(document.getElementById('tableTime')){
-    jsTableTime = parseInt(document.getElementById('tableTime').value);
+if (document.getElementById("tableTime")) {
+    jsTableTime = parseInt(document.getElementById("tableTime").value);
 }
-if (document.getElementById('graphTime')) {
-    jsGraphTime = parseInt(document.getElementById('graphTime').value);
+if (document.getElementById("graphTime")) {
+    jsGraphTime = parseInt(document.getElementById("graphTime").value);
 }
 function requestChartData() {
     $.ajax({
-        url: 'live-data.php',
+        url: "live-data.php",
         success: function(point) {
             var series = chart.series[0],
                 shift = series.data.length > 20; // shift if the series is
@@ -24,106 +24,156 @@ function requestChartData() {
 }
 
 function setStorageTotal(totalInQueueWorkers, totalRunningWorkers) {
-    var lastTotalRunningWorkers = parseInt(sessionStorage.getItem('totalRunningWorkers'));
-    var lastTotalInQueueWorkers = parseInt(sessionStorage.getItem('totalInQueueWorkers'));
+    var lastTotalRunningWorkers = parseInt(
+        sessionStorage.getItem("totalRunningWorkers")
+    );
+    var lastTotalInQueueWorkers = parseInt(
+        sessionStorage.getItem("totalInQueueWorkers")
+    );
     var diff = totalRunningWorkers - lastTotalRunningWorkers;
-    $('#totalInQueueWorkers').html(totalInQueueWorkers);
-    $('#totalInQueueDiff').html(diff);
-    $('#totalRunningWorkers').html(totalRunningWorkers);
+    $("#totalInQueueWorkers").html(totalInQueueWorkers);
+    $("#totalInQueueDiff").html(diff);
+    $("#totalRunningWorkers").html(totalRunningWorkers);
     if (totalInQueueWorkers) {
-        sessionStorage.setItem('totalInQueueWorkers', totalInQueueWorkers);
+        sessionStorage.setItem("totalInQueueWorkers", totalInQueueWorkers);
     }
     if (totalRunningWorkers) {
-        sessionStorage.setItem('totalRunningWorkers', totalRunningWorkers);
+        sessionStorage.setItem("totalRunningWorkers", totalRunningWorkers);
     }
 }
 
 function requestTableData(url, element) {
-    url += '&sort=' + $('#filterSort').val() + '&dir=' + $('#filterDir').val() + '&groupby=' + $('#filterGroupby').val();
+    url +=
+        "&sort=" +
+        $("#filterSort").val() +
+        "&dir=" +
+        $("#filterDir").val() +
+        "&groupby=" +
+        $("#filterGroupby").val();
     $.ajax({
-        method: 'GET',
+        method: "GET",
         url: url,
         success: function(info) {
-            try{
-                var obj = info
+            try {
+                var obj = info;
             } catch (e) {
                 var html = '<tr><td colspan="5">No jobs in queue</td></tr>';
-                $('#table > tbody').html(html);
+                $("#table > tbody").html(html);
                 return false;
             }
             var html = '<tr><td colspan="5">No jobs in queue</td></tr>';
-            if (obj['data']) {
+            if (obj["data"]) {
                 var totalInQueueWorkers = 0;
                 var totalRunningWorkers = 0;
-                if (obj['data'].length>0){
-                    html = '';
-                    for (var i = 0; i < obj['data'].length; i++) {
-                        totalInQueueWorkers += parseInt(obj['data'][i]['in_queue']);
-                        totalRunningWorkers += parseInt(obj['data'][i]['jobs_running']);
+                if (obj["data"].length > 0) {
+                    html = "";
+                    for (var i = 0; i < obj["data"].length; i++) {
+                        totalInQueueWorkers += parseInt(
+                            obj["data"][i]["in_queue"]
+                        );
+                        totalRunningWorkers += parseInt(
+                            obj["data"][i]["jobs_running"]
+                        );
 
-                        html += '<tr id="' + obj['data'][i]['id_key'] + 'w"' + (obj['data'][i]['capable_workers'] == 0 && obj['data'][i]['in_queue'] > 0 ? 'class="table-warning"':'')+'>';
-                        html += '<td><small>'+obj['data'][i]['server']+'</small></td>';
-                        html += '<td><small>'+obj['data'][i]['name']+'</small></td>';
-                        html += '<td id = "' + obj['data'][i]['id_key'] + 'in_queue">' + obj['data'][i]['in_queue']+'</td>';
-                        html += '<td id = "' + obj['data'][i]['id_key'] + 'jobs_running">' + obj['data'][i]['jobs_running']+'</td>';
-                        html += '<td id = "'+obj['data'][i]['id_key'] + 'capable_workers">';
-                        html += (obj['data'][i]['capable_workers'] == 0 && obj['data'][i]['in_queue'] > 0 ?'<img src="images/s_warn.png" />':'');
-                        html += obj['data'][i]['capable_workers'] +'</td>';
-                        html +'</tr>';
+                        html +=
+                            '<tr id="' +
+                            obj["data"][i]["id_key"] +
+                            'w"' +
+                            (obj["data"][i]["capable_workers"] == 0 &&
+                            obj["data"][i]["in_queue"] > 0
+                                ? 'class="table-warning"'
+                                : "") +
+                            ">";
+                        html +=
+                            "<td><small>" +
+                            obj["data"][i]["server"] +
+                            "</small></td>";
+                        html +=
+                            "<td><small>" +
+                            obj["data"][i]["name"] +
+                            "</small></td>";
+                        html +=
+                            '<td id = "' +
+                            obj["data"][i]["id_key"] +
+                            'in_queue">' +
+                            obj["data"][i]["in_queue"] +
+                            "</td>";
+                        html +=
+                            '<td id = "' +
+                            obj["data"][i]["id_key"] +
+                            'jobs_running">' +
+                            obj["data"][i]["jobs_running"] +
+                            "</td>";
+                        html +=
+                            '<td id = "' +
+                            obj["data"][i]["id_key"] +
+                            'capable_workers">';
+                        html +=
+                            obj["data"][i]["capable_workers"] == 0 &&
+                            obj["data"][i]["in_queue"] > 0
+                                ? '<img src="images/s_warn.png" />'
+                                : "";
+                        html += obj["data"][i]["capable_workers"] + "</td>";
+                        html + "</tr>";
                     }
                     setStorageTotal(totalInQueueWorkers, totalRunningWorkers);
-                    $('#table > tbody').html(html);
+                    $("#table > tbody").html(html);
                 }
             }
-            $('#table > tbody').html(html);
+            $("#table > tbody").html(html);
         }
     });
 }
 $(document).ready(function() {
     chart = new Highcharts.Chart({
         chart: {
-            renderTo: 'graph_1_second',
-            defaultSeriesType: 'spline',
+            renderTo: "graph_1_second",
+            defaultSeriesType: "spline",
             events: {
                 load: requestChartData
             }
         },
         title: {
-            text: '',
+            text: "",
             style: {
-                display: 'none'
+                display: "none"
             }
         },
         xAxis: {
-            type: 'datetime',
+            type: "datetime",
             tickPixelInterval: 150,
-            maxZoom: 20 * 1000,
+            maxZoom: 20 * 1000
         },
         yAxis: {
             minPadding: 0.2,
             maxPadding: 0.2,
             title: false
         },
-        series: [{
-            name: 'Total',
-            data: []
-        }]
+        series: [
+            {
+                name: "Total",
+                data: []
+            }
+        ]
     });
     setInterval(function() {
-        requestTableData('queue.php?json', '.result');
+        requestTableData("queue.php?json", ".result");
     }, jsTableTime);
-    $('.sortTable').click(
-        function () {
-            $('.sortTable').removeClass('up');
-            $('.sortTable').removeClass('down');
-
-            $(this).data('sort', ($(this).data('sort') == 'asc' ? 'desc' : 'asc'));
-            $(this).addClass(($(this).data('sort') === 'asc' ? 'up' : 'down'));
-            $('#filterSort').val($(this).data('id'));
-            $('#filterGroupby').val($(this).data('group'));
-            console.log($('#filterSort').val());
-            console.log($('#filterDir').val());
-        }
-    );
+    $(".sortTable").click(function() {
+        $(".sortTable")
+            .removeClass("up")
+            .removeClass("down");
+        $(this)
+            .data("sort", $(this).data("sort") == "asc" ? "desc" : "asc")
+            .addClass($(this).data("sort") === "asc" ? "up" : "down");
+        $("#filterDir").addClass(
+            $(this).data("sort") === "asc" ? "up" : "down"
+        );
+        $("#filterSort").val($(this).data("id"));
+        $("#filterGroupby").val($(this).data("group"));
+        console.log($("#filterSort").val());
+        console.log($("#filterDir").val());
+        requestTableData("queue.php?json", ".result");
+    });
 });
-requestTableData('queue.php?json', '.result');
+requestTableData("queue.php?json", ".result");
